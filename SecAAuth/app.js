7bfb8066906d1bb999cb74144df7efc763bc3b20
dynamicdev4all <149931467+dynamicdev4all@users.noreply.github.com>
2025-03-29 11:21:01 +0530

    Add files via upload

--- SecAAuth/app.js
+++ SecAAuth/app.js
@@ -1,35 +1,11 @@
 const express = require("express");
-
-const mongoose = require("mongoose");
-
 const app = express();
-
-mongoose
-  .connect(
-    "mongodb+srv://piebytwo014:piebytwo014@cluster0.n8om1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  )
-  .then(() => {
-    console.log("Database connection Success");
-  })
-  .catch((err) => {
-    console.log("Some issue in connection ", err);
-  });
-
 // app.use(express.static);
 
 const path = require("path");
-const { emit } = require("process");
-
+const dbconn = require("./config/databaseConnection");
+const { userRegister, userLogin } = require("./controllers/userController");
 app.use(express.urlencoded({ extended: true }));
-
-const userSchema = new mongoose.Schema({
-  userName: String,
-  email: String,
-  passsword: String,
-  isActive: Boolean,
-});
-
-const userModel = mongoose.model("User", userSchema);
 
 //this is the root route or home route
 app.get("/", (req, res) => {
@@ -46,43 +22,11 @@ app.get("/dashboard", (req, res) => {
 });
 app.post("/create-new-user", async (req, res) => {
   const { username, email, password } = req.body;
-  try {
-    const user = await userModel.findOne({ email: email });
-    if (user) {
-      console.log("This Email is already in use");
-    } else {
-      const newUser = new userModel({
-        userName: username,
-        email: email,
-        passsword: password,
-        isActive: false,
-      });
-      await newUser.save();
-      console.log("register success");
-    }
-  } catch (error) {
-    console.log(error);
-  }
+  userRegister(username, email, password)
 });
 app.post("/login-user", async (req, res) => {
   const { email, password } = req.body;
-  console.log(req.body);
-  try {
-    const user = await userModel.findOne({ email: email });
-    if (user) {
-      if (user.passsword == password && user.isActive) {
-        res.redirect("/dashboard");
-      } else if (user.passsword == password && !user.isActive) {
-        console.log("Please verify your email");
-      } else if (user.passsword != password) {
-        console.log("Password is invalid ");
-      }
-    } else {
-      console.log("The user does not exist");
-    }
-  } catch (error) {
-    console.log(error);
-  }
+  userLogin(email, password)
 });
 
 app.listen(1234, () => {
---
+++ SecAAuth/controllers/userController.js
@@ -0,0 +1,45 @@
+const userModel = require("../models/userModel");
+
+const userRegister = async(username, email, password)=>{
+    try {
+        const user = await userModel.findOne({ email: email });
+        if (user) {
+          console.log("This Email is already in use");
+        } else {
+          const newUser = new userModel({
+            userName: username,
+            email: email,
+            passsword: password,
+            isActive: false,
+          });
+          await newUser.save();
+          console.log("register success");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+}
+
+const userLogin = async(email, password)=>{
+    try {
+        const user = await userModel.findOne({ email: email });
+        if (user) {
+          if (user.passsword == password && user.isActive) {
+                 res.redirect("/dashboard");
+          } else if (user.passsword == password && !user.isActive) {
+            console.log("Please verify your email");
+          } else if (user.passsword != password) {
+            console.log("Password is invalid ");
+          }
+        } else {
+          console.log("The user does not exist");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+}
+
+module.exports = {
+    userRegister,
+    userLogin
+}
